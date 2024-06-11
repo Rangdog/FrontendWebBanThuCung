9986c8abf0cdf6dde755aff2fe4593e47f32ecdc
Rangdog <92283489+Rangdog@users.noreply.github.com>
2024-06-11 17:19:06 +0700

    fix import product

--- frontend/src/components/ImportProduct.jsx
+++ frontend/src/components/ImportProduct.jsx
@@ -1,6 +1,6 @@
 import React,{ useState, useEffect, useRef} from 'react';
-import { useForm } from 'react-hook-form';
-import { TextField, Button, Dialog, DialogActions, DialogContent, DialogTitle, Table, TableHead, TableBody, TableCell, TableRow, TableContainer, Paper, Box,Grid } from '@mui/material';
+import { get, useForm } from 'react-hook-form';
+import { TextField, Button, Dialog, DialogActions, DialogContent, DialogTitle, Table, TableHead, TableBody, TableCell, TableRow, TableContainer, Paper, Box,Grid,FormControl,InputLabel,Select,MenuItem } from '@mui/material';
 import AxiosInstance from './AxiosInstante';
 import { useSnackbar } from 'notistack';
 
@@ -19,18 +19,62 @@ const AddImportForm = ({ open, onClose, onSubmit }) => {
   const [openPetDialog, setOpenPetDialog] = useState(false);
   const [openProductDialog, setOpenProductDialog] = useState(false);
   const [items, setItems] = useState([]);
+  const [selectedItemIndex, setSelectedItemIndex] = useState(null);
+  const handleRowClick = (index) => {
+    if (selectedItemIndex == index){
+      setSelectedItemIndex(null);
+    }
+    else{
+      setSelectedItemIndex(index);
+    }
+  };
+  const handleDeleteItem = (index) => {
+    setItems(items.filter((_, i) => i !== index));
+  };
 
   const handleFormSubmit = (data) => {
     onSubmit({ ...data, items });
   };
 
   const handleAddPet = (petData) => {
-    setItems([...items, petData]);
+    setItems(prevItems => {
+      const existingItemIndex = prevItems.findIndex(item => 
+        item.type === "Thú cưng" &&
+        item.loaiThuCung.maThuCung === petData.loaiThuCung.maThuCung &&
+        item.price === petData.price
+      );
+  
+      if (existingItemIndex !== -1) {
+        // Update the quantity of the existing item
+        const updatedItems = [...prevItems];
+        updatedItems[existingItemIndex].quantity  = parseInt(updatedItems[existingItemIndex].quantity,10) + parseInt(petData.quantity,10);
+        return updatedItems;
+      } else {
+        // Add new item
+        return [...prevItems, petData];
+      }
+    });
     setOpenPetDialog(false);
   };
 
   const handleAddProduct = (productData) => {
-    setItems([...items, productData]);
+    setItems(prevItems => {
+      const existingItemIndex = prevItems.findIndex(item => 
+        item.type === "Sản phẩm" &&
+        item.loaiSanPham.maSanPham === productData.loaiSanPham.maSanPham &&
+        item.price === productData.price
+      );
+  
+      if (existingItemIndex !== -1) {
+        // Update the quantity of the existing item
+        const updatedItems = [...prevItems];
+        updatedItems[existingItemIndex].quantity = parseInt(updatedItems[existingItemIndex].quantity,10) + parseInt(productData.quantity,10);
+        return updatedItems;
+      } else {
+        // Add new item
+        return [...prevItems, productData];
+      }
+    });
     setOpenProductDialog(false);
   };
 
@@ -62,11 +106,26 @@ const AddImportForm = ({ open, onClose, onSubmit }) => {
                 </TableHead>
                 <TableBody>
                   {items.map((item, index) => (
-                    <TableRow key={index}>
+                    <TableRow key={index}
+                     onClick={() => handleRowClick(index)}
+                      selected={selectedItemIndex === index}
+                      style={{ cursor: 'pointer' }}>
                       <TableCell>{item.type}</TableCell>
-                      <TableCell>{item.name}</TableCell>
+                      <TableCell>{item.type === "Thú cưng"? item.loaiThuCung.tenThuCung : item.loaiSanPham.tenSanPham}</TableCell>
                       <TableCell>{item.quantity}</TableCell>
                       <TableCell>{item.price}</TableCell>
+                      <TableCell>
+                        <Button
+                          variant="contained"
+                          color="error"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            handleDeleteItem(index);
+                          }}
+                        >
+                          Xóa
+                        </Button>
+                      </TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
@@ -87,24 +146,62 @@ const AddImportForm = ({ open, onClose, onSubmit }) => {
 
 const PetDialog = ({ open, onClose, onSubmit }) => {
   const { register, handleSubmit, formState: { errors } } = useForm();
+  const [selectedPet, setSelectedPet] = useState(null);
+  const [error, setError] = useState(false);
+  const [items,setItems] = useState([])
+  const getPet = async()=>{
+    try{
+        const res = await AxiosInstance.get("center/thucung")
+        if(res.status === 200){
+          const productItems = res.data.map(item => ({
+            maThuCung: item.maThuCung,
+            tenThuCung: item.tenThuCung
+          }));
+          setItems(productItems);
+        }
+    }
+    catch(err){
+      console.error(err)
+    }
+  }
 
   const handleFormSubmit = (data) => {
-    onSubmit({ ...data, type: 'Thú cưng' });
+    if(!selectedPet){
+      setError('loaiThuCung', { type: 'required', message: 'Trường này là bắt buộc' });
+    }
+    onSubmit({ ...data, loaiThuCung:selectedPet ,type: 'Thú cưng' });
   };
+
+  const handlePetSelect = (event) => {
+    const selectedPet = items.find(i => i.maThuCung === event.target.value);
+    setSelectedPet(selectedPet);
+  };
+  useEffect(()=>{
+    getPet();
+  },[])
 
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Thêm thú cưng</DialogTitle>
       <DialogContent>
         <form onSubmit={handleSubmit(handleFormSubmit)} noValidate>
-          <TextField
-            margin="dense"
-            label="Tên thú cưng"
-            fullWidth
-            {...register("name", { required: true })}
-            error={!!errors.name}
-            helperText={errors.name ? "Tên thú cưng là bắt buộc" : ""}
-          />
+        <FormControl fullWidth error={error} margin="dense">
+              <InputLabel id="pet-select-label">Thú cưng</InputLabel>
+                    <Select
+                      labelId="pet-select-label"
+                      id="product-select"
+                      value={selectedPet ? selectedPet.maThuCung : ''}
+                      onChange={handlePetSelect}
+                      required
+                    >
+                      {items.map((i) => (
+                        <MenuItem key={i.maThuCung} value={i.maThuCung}>
+                          {i.tenThuCung}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                {error && <FormHelperText>Trường này là bắt buộc</FormHelperText>}
+          </FormControl>
           <TextField
             margin="dense"
             label="Số lượng"
@@ -135,24 +232,59 @@ const PetDialog = ({ open, onClose, onSubmit }) => {
 
 const ProductDialog = ({ open, onClose, onSubmit }) => {
   const { register, handleSubmit, formState: { errors } } = useForm();
-
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [error, setError] = useState(false);
+  const [items,setItems] = useState([])
+  const getSanPham = async()=>{
+    try{
+        const res = await AxiosInstance.get("center/sanpham")
+        if(res.status === 200){
+          const productItems = res.data.map(item => ({
+            maSanPham: item.maSanPham,
+            tenSanPham: item.tenSanPham
+          }));
+          setItems(productItems);
+        }
+    }
+    catch(err){
+      console.error(err)
+    }
+  }
   const handleFormSubmit = (data) => {
-    onSubmit({ ...data, type: 'Sản phẩm' });
+    if(!selectedProduct){
+      setError('loaiSanPham', { type: 'required', message: 'Trường này là bắt buộc' });
+    }
+    onSubmit({ ...data,loaiSanPham:selectedProduct , type: 'Sản phẩm' });
   };
-
+  const handleProductSelect = (event) => {
+    const selectedProduct = items.find(i => i.maSanPham === event.target.value);
+    setSelectedProduct(selectedProduct);
+  };
+  useEffect(()=>{
+    getSanPham();
+  },[])
   return (
-    <Dialog open={open} onClose={onClose}>
+    <Dialog open={open} onClose={onClose} fullWidth maxWidth="lg">
       <DialogTitle>Thêm sản phẩm</DialogTitle>
       <DialogContent>
         <form onSubmit={handleSubmit(handleFormSubmit)} noValidate>
-          <TextField
-            margin="dense"
-            label="Tên sản phẩm"
-            fullWidth
-            {...register("name", { required: true })}
-            error={!!errors.name}
-            helperText={errors.name ? "Tên sản phẩm là bắt buộc" : ""}
-          />
+          <FormControl fullWidth error={error} margin="dense">
+              <InputLabel id="pet-select-label">Sản phẩm</InputLabel>
+                    <Select
+                      labelId="pet-select-label"
+                      id="product-select"
+                      value={selectedProduct ? selectedProduct.maSanPham : ''}
+                      onChange={handleProductSelect}
+                      required
+                    >
+                      {items.map((i) => (
+                        <MenuItem key={i.maSanPham} value={i.maSanPham}>
+                          {i.tenSanPham}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                {error && <FormHelperText>Trường này là bắt buộc</FormHelperText>}
+          </FormControl>
           <TextField
             margin="dense"
             label="Số lượng"
@@ -326,22 +458,88 @@ const ManageAccount = () => {
   };
 
   const handleFormSubmit = async(data) => {
-    const chinhanh = data.tenChiNhanh.replace("\"","");
-    console.log(chinhanh)
+    const maNV = localStorage.getItem('tenDangNhap')
+    const pet = []
+    const product = []
+    for (const i in data.items){
+      if(data.items[i].type === "Thú cưng"){
+        pet.push(data.items[i])
+      }
+      else{
+        product.push(data.items[i])
+      }
+    }
+    console.log(pet)
+    console.log(product)
     try{
-      const res = await AxiosInstance.post("/center/chinhanh", chinhanh,{
-        headers:{
-          'Content-Type':'text/plain'
+      const getNhanvien = await AxiosInstance.get(`identity/nhanvien/${maNV}`)
+      if(getNhanvien.status === 200){
+       const machinhanh = getNhanvien.data.maChiNhanh
+       console.log(machinhanh)
+       try{
+        const getChiNhanh =  await AxiosInstance.get(`center/chinhanh/${machinhanh}`)
+        if(getChiNhanh.status === 200){
+          console.log(getChiNhanh.data)
+          try{
+            const res = await AxiosInstance.post("center/nhaphang/tao-phieu-nhap",{
+              "maNhanVien": maNV,
+              "chiNhanhDTO": getChiNhanh.data
+            })
+            if(res.status === 200){
+              console.log(1)
+              const datathucung = []
+              const dataSP = []
+              for (const i in pet){
+                datathucung.push({
+                  "maDonNhap": res.data.maDonNhapHang,
+                  "maThuCung": pet[i].loaiThuCung.maThuCung,
+                  "giaNhap": pet[i].price,
+                  "soLuong": pet[i].quantity
+                })
+              }
+              console.log(datathucung)
+              for (const i in product){
+                dataSP.push({
+                  "maDonNhap": res.data.maDonNhapHang,
+                  "maSanPham": product[i].loaiSanPham.maSanPham,
+                  "giaNhap": product[i].price,
+                  "soLuong": product[i].quantity
+                })
+              }
+              console.log(dataSP)
+              console.log(datathucung)
+              try{
+                const addthucung = await AxiosInstance.post("center/nhaphang/them-thu-cung",datathucung)
+                if(addthucung.status === 200){
+                  try{
+                    const addSP = await AxiosInstance.post("center/nhaphang/them-san-pham",dataSP)
+                    if(addSP.status === 200){
+                      enqueueSnackbar('Hoàn thành nhập hàng', {variant : 'success', autoHideDuration: 3000} )
+                      getNhapHang()
+                    }
+                  }
+                  catch(e){
+                    enqueueSnackbar('Lỗi thêm sản phẩm', {variant : 'error', autoHideDuration: 3000} )
+                  }
+                }
+              }
+              catch(e){
+                enqueueSnackbar('Lỗi thêm thú cưng', {variant : 'error', autoHideDuration: 3000} )
+              }
+            }
+          }
+          catch(err){
+            enqueueSnackbar('Lỗi tạo phiếu nhập', {variant : 'error', autoHideDuration: 3000} )
+          }   
         }
-      })
-      if(res.status === 200){
-        enqueueSnackbar('Thêm chi nhánh thành công', {variant : 'success', autoHideDuration: 3000} )
-        getChiNhanh()
+      }
+      catch(err){
+        enqueueSnackbar('Lỗi lấy chi nhanh nhân viên', {variant : 'error', autoHideDuration: 3000} )
+      }
       }
     }
     catch(err){
-      enqueueSnackbar('Lỗi', {variant : 'error', autoHideDuration: 3000} )
-      console.log(err)
+      enqueueSnackbar('Lỗi lấy nhân viên', {variant : 'error', autoHideDuration: 3000} )
     }
     handleDialogClose();
   };
@@ -372,7 +570,6 @@ const ManageAccount = () => {
     return (
       <div>
         <Button variant="contained" color="primary" style={{marginRight: '10px'}} onClick={onOpenDialog}>Thêm</Button>
-        <Button variant="contained" color="primary" style={{marginRight: '10px'}} onClick={onOpenEditForm} disabled={isDisabled}>Xem chi tiết</Button>
       </div>
     );
   };
